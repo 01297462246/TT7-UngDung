0df5365a8dda5dc22587a6a9a921d223621b94d0
Huỳnh Duy Quốc <43953469+01297462246@users.noreply.github.com>
2019-09-13 11:00:43 +0700

    Add files via upload

---
+++ w2_Cau1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a, b, x;
+            Console.WriteLine("Nhap a: ");
+            a = float.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap b: ");
+            b = float.Parse(Console.ReadLine());
+            Console.WriteLine(a + "x + " + b + " = 0");
+            Console.WriteLine("x = {0}", (-b) / a);
+            Console.ReadKey();
+        }
+    }
+}
