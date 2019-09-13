42e9f97107de4a64b89de433501777667caf2ee7
Huỳnh Duy Quốc <43953469+01297462246@users.noreply.github.com>
2019-09-13 11:01:58 +0700

    Add files via upload

---
+++ w2_Cau4.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhap gio: ");
+            int nH = int.Parse(Console.ReadLine());
+            if((nH>=0 && nH <4) || (nH>=20 && nH <=24))
+            {
+                Console.WriteLine("Good night!");
+            }
+            else if(nH >=4 && nH<=12)
+            {
+                Console.WriteLine("Good morning!");
+            }
+            else if(nH>12 && nH<=17)
+            {
+                Console.WriteLine("Good afternoon!");
+            }
+            else if(nH>17 && nH<20)
+            {
+                Console.WriteLine("Good evening!");
+            }
+            else
+            {
+                Console.WriteLine("Default!!!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
