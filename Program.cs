91d6cc312c72ad6bc9fb6f81f8a1acccacdb6f43
Huỳnh Duy Quốc <43953469+01297462246@users.noreply.github.com>
2019-09-20 10:13:32 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nN;
+            Console.WriteLine("Nhap N: ");
+            nN = int.Parse(Console.ReadLine());
+            int i = -nN;
+            do
+            {
+                Console.Write(" {0}", i);
+                i++;
+            } while (i <= nN);
+            Console.ReadKey();
+        }
+    }
+}
